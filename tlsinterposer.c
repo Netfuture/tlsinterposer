a0967d2963230a285273b715639c0bbca0fea976
Marcel Waldvogel <waldvogel@netfuture.ch>
2013-11-15 07:51:08 +0100

    Cleanup

--- tlsinterposer.c
+++ tlsinterposer.c
@@ -5,11 +5,11 @@
 #include <dlfcn.h>
 
 #ifdef DEBUG
-#define LOG(x)     fprintf(stderr, (x))
-#define LOG2(x, y) fprintf(stderr, (x), (y))
+#define DEBUGLOG(x)     fprintf(stderr, (x))
+#define DEBUGLOG2(x, y) fprintf(stderr, (x), (y))
 #else
-#define LOG(x)
-#define LOG2(x, y)
+#define DEBUGLOG(x)
+#define DEBUGLOG2(x, y)
 #endif
 
 static void *ssl_dlsym(const char *name)
@@ -30,7 +30,7 @@ static void *ssl_dlsym(const char *name)
 static int interposer_SSL_CTX_set_cipher_list(SSL_CTX *ctx, const char *str)
 {
 	int (*orig_SSL_CTX_set_cipher_list)(SSL_CTX *, const char *);
-	LOG("libtlsinterposer.so:interposer_SSL_CTX_set_cipher_list() starting\n");
+	DEBUGLOG("libtlsinterposer.so:interposer_SSL_CTX_set_cipher_list() starting\n");
 	orig_SSL_CTX_set_cipher_list = ssl_dlsym("SSL_CTX_set_cipher_list");
 	if (orig_SSL_CTX_set_cipher_list == NULL) {
 		fprintf(stderr, "libtlsinterposer.so:interposer_SSL_CTX_set_cipher_list() cannot find SSL_CTX_set_cipher_list()\n");
@@ -47,7 +47,7 @@ static int default_SSL_CTX_set_cipher_list(SSL_CTX *ctx)
 		// - https://community.qualys.com/blogs/securitylabs/2013/08/05/configuring-apache-nginx-and-openssl-for-forward-secrecy
 		ciphers = "EECDH+ECDSA+AESGCM:EECDH+aRSA+AESGCM:EECDH+ECDSA+SHA384:EECDH+ECDSA+SHA256:EECDH+aRSA+SHA384:EECDH+aRSA+SHA256:EECDH+aRSA+RC4:EECDH:EDH+aRSA:RC4:!aNULL:!eNULL:!LOW:!3DES:!MD5:!EXP:!PSK:!SRP:!DSS:!RC4";
 	}
-	LOG2("libtlsinterposer.so:default_SSL_CTX_set_cipher_list() using %s\n", ciphers);
+	DEBUGLOG2("libtlsinterposer.so:default_SSL_CTX_set_cipher_list() using %s\n", ciphers);
 	return interposer_SSL_CTX_set_cipher_list(ctx, ciphers);
 }
 
@@ -69,17 +69,17 @@ SSL_CTX *SSL_CTX_new(const SSL_METHOD *method)
 	if (ctx != NULL) {
 		SSL_CTX_set_options(ctx, SSL_OP_NO_SSLv2 | SSL_OP_NO_SSLv3);
 		default_SSL_CTX_set_cipher_list(ctx);
-		// Taken from Vincent Bernat
+		// Based on code by Vincent Bernat
 		// - http://vincent.bernat.im/en/blog/2011-ssl-perfect-forward-secrecy.html
 		// - https://github.com/bumptech/stud/pull/61
 #ifdef NID_X9_62_prime256v1
-		EC_KEY *ecdh = NULL;
+		EC_KEY *ecdh;
 		ecdh = EC_KEY_new_by_curve_name(NID_X9_62_prime256v1);
-		SSL_CTX_set_tmp_ecdh(ctx,ecdh);
+		SSL_CTX_set_tmp_ecdh(ctx, ecdh);
 		EC_KEY_free(ecdh);
-		LOG("libtlsinterposer.so:ECDH Initialized with NIST P-256\n");
+		DEBUGLOG("libtlsinterposer.so:ECDH Initialized with NIST P-256\n");
 #endif
 	}
-	LOG2("libtlsinterposer.so:SSL_CTX_new returning %p\n", ctx);
+	DEBUGLOG2("libtlsinterposer.so:SSL_CTX_new returning %p\n", ctx);
 	return ctx;
 }
