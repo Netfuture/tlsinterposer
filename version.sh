5fd3dc61248e4fba6d04f2933726ae832143653b
Marcel Waldvogel <waldvogel@netfuture.ch>
2014-06-25 13:47:01 +0200

    No longer requires .git to compile

--- version.sh
+++ version.sh
@@ -2,11 +2,13 @@
 # Try go get the version from git; fall back to CHANGES.txt
 version=`sed -n -e 's/^\([0-9]*\.[0-9]*\.[0-9]*\).*/\1/p' CHANGES.txt | tail -1`
 if which git > /dev/null; then
+    if -d .git; then
         gitv=`git describe --tags | sed -e 's/^v\([0-9.]*\)-*.*/\1/'`
         if [ x$gitv != x$version ]; then
-                echo $gitv
-                echo "WARNING: git and CHANGES.txt versions disagree" >&2
-                exit 0
+            echo $gitv
+            echo "WARNING: git and CHANGES.txt versions disagree" >&2
+            exit 0
         fi
+    fi
 fi
 echo $version
