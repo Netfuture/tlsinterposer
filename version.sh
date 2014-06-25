99641232ce9cda3fb449abe122b9755ca9cec555
Marcel Waldvogel <waldvogel@netfuture.ch>
2014-06-25 22:02:01 +0200

    Use [ -d

--- version.sh
+++ version.sh
@@ -2,7 +2,7 @@
 # Try go get the version from git; fall back to CHANGES.txt
 version=`sed -n -e 's/^\([0-9]*\.[0-9]*\.[0-9]*\).*/\1/p' CHANGES.txt | tail -1`
 if which git > /dev/null; then
-    if -d .git; then
+    if [ -d .git ]; then
         gitv=`git describe --tags | sed -e 's/^v\([0-9.]*\)-*.*/\1/'`
         if [ x$gitv != x$version ]; then
             echo $gitv
